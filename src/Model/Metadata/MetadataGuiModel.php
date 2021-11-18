06724869bd3516cd4c05474a79de1f5b052d7592
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2021-11-18 10:51:43 +0100

    #17271

--- src/Model/Metadata/MetadataGuiModel.php
+++ src/Model/Metadata/MetadataGuiModel.php
@@ -17,26 +17,23 @@ class MetadataGuiModel extends \Drupal\arche_gui_api\Model\ArcheApiModel
     public function getOntology(): array
     {
         $dbconnStr = yaml_parse_file(\Drupal::service('extension.list.module')->getPath('acdh_repo_gui').'/config/config.yaml')['dbConnStr']['guest'];
+        $schema = $this->repo->getSchema();
+        
         $conn = new \PDO($dbconnStr);
         $cfg = (object) [
             //'skipNamespace'     => $this->properties->baseUrl.'%', // don't forget the '%' at the end!
-            'ontologyNamespace' => $this->repo->getSchema()->namespaces->ontology,
-            'parent'            => $this->repo->getSchema()->namespaces->ontology.'isPartOf',
-            'label'             => $this->repo->getSchema()->namespaces->ontology.'hasTitle',
-            //'order'             => 'https://vocabs.acdh.oeaw.ac.at/schema#ordering',
-            //'cardinality'       => 'https://vocabs.acdh.oeaw.ac.at/schema#cardinality',
-            //'recommended'       => 'https://vocabs.acdh.oeaw.ac.at/schema#recommendedClass',
-            //'langTag'           => 'https://vocabs.acdh.oeaw.ac.at/schema#langTag',
-            //'vocabs'            => 'https://vocabs.acdh.oeaw.ac.at/schema#vocabs',
-            //'altLabel'          => 'http://www.w3.org/2004/02/skos/core#altLabel'
+            'ontologyNamespace' => $schema->namespaces->ontology,
+            'parent'            => $schema->parent,
+            'label'             => $schema->label
         ];
        
         $ontology = new \acdhOeaw\arche\lib\schema\Ontology($conn, $cfg);
         
-        $collectionProp = $ontology->getClass($this->repo->getSchema()->namespaces->ontology.'Collection')->properties;
-        $projectProp = $ontology->getClass($this->repo->getSchema()->namespaces->ontology.'Project')->properties;
-        $resourceProp = $ontology->getClass($this->repo->getSchema()->namespaces->ontology.'Resource')->properties;
-        
-        return array('collection' => $collectionProp, 'project' => $projectProp, 'resource' => $resourceProp);
+        $classesDesc = [];
+        foreach (['collection', 'topCollection', 'resource', 'project'] as $i) {
+            $classesDesc[$i] = $ontology->getClass($schema->classes->$i)->properties ?? "";
+        }
+        return $classesDesc;
+       
     }
 }
