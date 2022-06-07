323cbb3d311b0ab41692c58ae2360036bc9e190b
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2022-06-07 13:38:20 +0200

    versions block moved to api endpoint

--- src/Controller/VersionsController.php
+++ src/Controller/VersionsController.php
@@ -3,6 +3,7 @@
 namespace Drupal\arche_gui_api\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Description of VersionsController
@@ -11,6 +12,12 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class VersionsController
 {
+    /**
+     * Create Object for the versions tree view
+     * @param string $repoid
+     * @param string $lang
+     * @return JsonResponse
+     */
     public function execute(string $repoid, string $lang = "en"): JsonResponse
     {
         /*
@@ -32,5 +39,74 @@ class VersionsController
         }
 
         return new JsonResponse($content, 200, ['Content-Type' => 'application/json']);
+    }
+    
+   
+    /**
+     * Create the normal list view API for the versions
+     * @param string $repoid
+     * @param string $lang
+     * @return Response
+     */
+    public function executeList(string $repoid, string $lang = "en"): Response
+    {
+        /*
+         * Usage:
+         *  https://domain.com/browser/api/versions_list/repoid/lang?_format=json
+         */
+
+        $repoid = \Drupal\Component\Utility\Xss::filter(preg_replace('/[^0-9]/', '', $repoid));
+     
+        if (empty($repoid)) {
+            return new Response(array("Please provide a repoid string"), 404, ['Content-Type' => 'application/json']);
+        }
+        
+        $blockModel = new \Drupal\acdh_repo_gui\Model\BlocksModel();
+        $params = array('identifier' => $repoid, 'lang' => $lang);
+        $data = $blockModel->getViewData("versions", $params);
+        
+        if (count((array) $data) > 1) {
+            if ($data[0]->id == $repoid) {
+                $data = [];
+            }
+        } else {
+           $data = []; 
+        }
+       
+        $build = [
+            '#theme' => 'acdh-repo-gui-detail-versions-block',
+            '#result' => (array) $data,
+            '#cache' => ['max-age' => 0],
+            '#attached' => [
+                'library' => [
+                    'acdh_repo_gui/repo-styles',
+                ]
+            ]
+        ];
+       
+        return new Response(render($build));
+        
+        
+    }
+    
+    
+    
+    /**
+     * Check if the actual resource has a newer version
+     * @param string $id
+     * @return bool
+     */
+    private function checkVersions(string $id): string
+    {
+        
+        echo '<pre>';
+        var_dump($data);
+        echo '</pre>';
+        if (count((array) $data) > 1) {
+            if ($data[0]->id != $id) {
+                return $data[0]->id;
+            }
+        }
+        return "";
     }
 }
