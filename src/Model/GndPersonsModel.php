24071b164e694315c123e15e56eb64b1eb884a1f
Norbert Czirjak <Norbert.Czirjak@oeaw.ac.at>
2023-08-14 12:21:43 +0200

    gnd api changed to arche core api

--- src/Model/GndPersonsModel.php
+++ src/Model/GndPersonsModel.php
@@ -7,44 +7,60 @@ namespace Drupal\arche_gui_api\Model;
  *
  * @author nczirjak
  */
-class GndPersonsModel extends \Drupal\arche_gui_api\Model\ArcheApiModel
-{
-    public function __construct()
-    {
+class GndPersonsModel extends \Drupal\arche_gui_api\Model\ArcheApiModel {
+
+    private $repo;
+
+    public function __construct() {
         parent::__construct();
+        $this->repo = \acdhOeaw\arche\lib\Repo::factory($this->config);
     }
-    
-    public function getData(): array
-    {
-        $requestUrl = $this->repoDb->getBaseUrl() . "search?"
-                . $this->getQueryPropVal() . ""
-                . "&lang[]=" . $this->siteLang;
-         
-        try {
-            $graph = new \EasyRdf\Graph();
-            return $graph->newAndLoad($requestUrl)->toRdfPhp();
-        } catch (\Exception | \EasyRdf\Exception  $ex) {
-            die($ex->getMessage());
-            \Drupal::logger('arche_rest_gui')->notice($ex->getMessage());
-            return array();
+
+    /**
+     * Check the gnd and acdhid
+     * @param array $ids
+     * @return string
+     */
+    private function checkId(array $ids): string {
+        $gndId = "";
+        $acdhId = "";
+        
+        foreach($ids as $id) {
+            if (strpos($id, 'd-nb.info/gnd/') !== false) {
+                $gndId = $id;
+            }
+            if (strpos($id, 'https://id.acdh.oeaw.ac.at/') !== false) {
+                $acdhId = $id;
+            }
         }
-        return array();
+        
+        if(!empty($gndId) && !empty($acdhId)) {
+            return $gndId.'|'.$acdhId.PHP_EOL;
+        }
+        return "";
     }
+
+    /**
+     * Get the graphs and return the result as a string
+     * @return string
+     */
+    public function getData(): string {
+
+        $config = new \acdhOeaw\arche\lib\SearchConfig();
+        $searchTerms[] = new \acdhOeaw\arche\lib\SearchTerm($this->repoDb->getSchema()->__get('namespaces')->rdfs . "type", $this->repoDb->getSchema()->__get('namespaces')->ontology . 'Person', '=');
+
+        $results = $this->repo->getResourcesBySearchTerms($searchTerms, $config);
+        $idProp = $this->repoDb->getSchema()->__get('namespaces')->ontology . 'hasIdentifier';
+           
+        $str = "";
+        foreach ($results as $r) {
+            $check = $this->checkId($r->getGraph()->allResources($idProp));
+            if(!empty($check)) {
+                $str .= $check;
+            }
+        }
+        return $str;
+    }
+
     
-    private function getQueryPropVal(): string
-    {
-        return http_build_query(
-            array(
-                'property' => array(
-                    $this->repoDb->getSchema()->__get('namespaces')->rdfs . "type",
-                    $this->repoDb->getSchema()->__get('namespaces')->ontology . "hasIdentifier"
-                ),
-                'value' => array(
-                    $this->repoDb->getSchema()->__get('namespaces')->ontology.'Person',
-                    'gnd'
-                ),
-                'operator' => array('=', '~')
-            )
-        );
-    }
 }
--- src/Object/GndPersonsObject.php
+++ src/Object/GndPersonsObject.php
@@ -25,7 +25,7 @@ class GndPersonsObject extends \Drupal\arche_gui_api\Object\MainObject
         return $this->createGNDFile($this->model->getData());
     }
 
-    private function createGNDFile(array $data): array
+    private function createGNDFile(string $data): array
     {
         $this->createFileLocation();
         return $this->processData($data);
@@ -37,24 +37,18 @@ class GndPersonsObject extends \Drupal\arche_gui_api\Object\MainObject
         $this->fileLocation = \Drupal::request()->getSchemeAndHttpHost() . '/browser/sites/default/files/beacon.txt';
     }
 
-    private function processData(array $data): array
+    private function processData(string $data): array
     {
-        if (count((array) $data) > 0) {
-            foreach ($data as $val) {
-                if (count($val[$this->repoDb->getSchema()->id] ?? []) > 0) {
-                    //$this->text .= $this->getGNDIdentifier($val[$this->repoDb->getSchema()->id]) . "|" . $this->host . $this->getRepoId($val[$this->repoDb->getSchema()->id]) . " \n";
-                    $this->text .= $this->getGNDIdentifier($val[$this->repoDb->getSchema()->id]) . "|" . $this->getResourceUrl($val[$this->repoDb->getSchema()->id]) . " \n";
-                }
-            }
-            return $this->createFileContent();
+        if (!empty($data)) {
+            return $this->createFileContent($data);
         }
         return array();
     }
 
-    private function createFileContent(): array
+    private function createFileContent(string $str): array
     {
-        if (!empty($this->text)) {
-            $this->text = "#FORMAT: BEACON \n" . $this->text;
+        if (!empty($str)) {
+            $this->text = "#FORMAT: BEACON \n" . $str;
             if (file_save_data($this->text, "public://beacon.txt", \Drupal\Core\File\FileSystemInterface::EXISTS_REPLACE)) {
                 return array('status' => 'File created', 'url' => $this->fileLocation);
             } else {
@@ -65,64 +59,4 @@ class GndPersonsObject extends \Drupal\arche_gui_api\Object\MainObject
         }
     }
     
-    /**
-     * Get the GND identifier from the data array
-     * @param array $ids
-     * @return string
-     */
-    private function getGNDIdentifier(array $ids): string
-    {
-        foreach ($ids as $id) {
-            if (strpos($id['value'], '/gnd/') !== false) {
-                return $id['value'];
-            }
-        }
-        return "";
-    }
-    
-    /**
-     * Get the ARCHE GUI Detail view url
-     * @param array $ids
-     * @return int
-     */
-    private function getRepoId(array $ids): int
-    {
-        foreach ($ids as $id) {
-            if (str_starts_with($id['value'], $this->repoDb->getBaseUrl())) {
-                return (int)str_replace($this->repoDb->getBaseUrl(), "", $id['value']);
-            }
-        }
-        return 0;
-    }
-    
-    /**
-     * Return the id.acdh.oeaw.ac.at/pid/gui url
-     * @param array $ids
-     * @return string
-     */
-    private function getResourceUrl(array $ids): string
-    {
-        $urls = $this->fetchUrls($ids);
-        return isset($urls['acdhId']) ? $urls['acdhId'] : (isset($urls['pid']) ? $urls['pid'] : (isset($urls['acdhGuiId']) ? $urls['acdhGuiId'] : ""));
-    }
-    
-    /**
-     * Collect the urls from the identifiers array
-     * @param array $ids
-     * @return array
-     */
-    private function fetchUrls(array $ids): array
-    {
-        $result = [];
-        foreach ($ids as $id) {
-            if (str_starts_with($id['value'], $this->repoDb->getSchema()->namespaces->id)) {
-                $result['acdhId'] = $id['value'];
-            } elseif (str_starts_with($id['value'], $this->repoDb->getSchema()->drupal->epicResolver)) {
-                $result['pid'] = $id['value'];
-            } else {
-                $result['acdhGuiId'] = $this->host . $this->getRepoId($ids);
-            }
-        }
-        return $result;
-    }
 }
